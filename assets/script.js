709c1019504eadbfdd857e751311dadf0cb2e59c
Ye0mi <wonit@snu.ac.kr>
2023-06-02 05:20:47 +0900

    w

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+function goToLink(linkId) {
+  switch (linkId) {
+    case 'link1':
+      window.location.href = "cake.html";
+      break;
+    case 'link2':
+      window.location.href = 'bdaycam.html';
+      break;
+    case 'link3':
+      window.location.href = 'https://example.com/link3';
+      break;
+    case 'link4':
+      window.location.href = 'https://example.com/link4';
+      break;
+    default:
+      break;
+  }
+}
